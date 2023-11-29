337385b2843eafe0bf2f84b42ebfac086a35ae2e
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-11-29 10:07:34 +0600

    adding side Nav

---
+++ Portfolio/src/Components/SideNav.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+const SideNav = () => {
+    return (
+        <div>
+            <div>SideNav</div>
+            
+        </div>
+    );
+};
+
+export default SideNav;
